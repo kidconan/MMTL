e2b1964d729b65d9dad7485947a7ed790c973f9d
kidconan <hyskid801@126.com>
2021-01-08 11:01:53 +0800

    Update reptile_meta_train.py

--- reptile_meta_train.py
+++ reptile_meta_train.py
@@ -441,16 +441,16 @@ def main(env_name, ckt_path, cross_idx_list, crossprefix, net_conf, cnn2rnn, rnn
 			torch.cuda.manual_seed_all(seed)
 		torch.backends.cudnn.deterministic = True
 
-		asr_model = CRNN(out_dim=4, cnn_setting=cnn_setting, rnn_setting=rnn_setting,
+		ser_model = CRNN(out_dim=4, cnn_setting=cnn_setting, rnn_setting=rnn_setting,
 						 pooling_setting=pooling_setting, dnn_setting=dnn_setting, dropout_rate=drop_rate,
 						 cnn2rnn=cnn2rnn, rnn2dnn=rnn2dnn)
 
-		asr_model.cuda(gpus[0])
+		ser_model.cuda(gpus[0])
 
-		train(model=asr_model, cross_idx=cross_idx, cross_path=cross_path, vis=vis, learning_rate=paras.outer_lr,
+		train(model=ser_model, cross_idx=cross_idx, cross_path=cross_path, vis=vis, learning_rate=paras.outer_lr,
 			  ckp_pth=ckt_path, penlty_bool=penlty_bool)
 
-		del asr_model
+		del ser_model
 		print("=================================================Finish the leave %d speaker programming"
 			  "=================================================" % j)
 		print()
